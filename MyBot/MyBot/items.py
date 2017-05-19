57fa78c7ef68cba3068f313f32a906d331aa2828
ashwalnavada <ashwal.kudremukh@gmail.com>
2017-05-19 10:52:19 -0400

    updated items.py

--- MyBot/MyBot/items.py
+++ MyBot/MyBot/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class MybotItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Course(scrapy.Item):
+    site = scrapy.Field()
+    institute = scrapy.Field()
+    id = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    credits = scrapy.Field()
+    category = scrapy.Field()
